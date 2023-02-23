40f004d888e4b379c87613b55dff8bc353de9927
깃waimi3169 <waimi3169@naver.com>
2023-02-23 10:48:19 +0900

    문제 ãp120807풀이

---
+++ src/main/java/com/ll/level0/p120807/Main.java
@@ -0,0 +1,16 @@
+package com.ll.level0.p120807;
+
+public class Main {
+    public static void main(String[] args) {
+        /*System.out.println(new Solution().solution(2,3));
+        System.out.println(new Solution().solution(11,11));
+        System.out.println(new Solution().solution(7,99));*/
+
+    }
+}
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1 == num2 ?1:-1;
+    }
+}
