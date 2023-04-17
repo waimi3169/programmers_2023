673546c3f8d6dc380737f967e1b68e28a51afbde
깃waimi3169 <waimi3169@naver.com>
2023-04-17 14:04:52 +0900

    p87377 교점 구현

---
+++ src/main/java/com/ll/level0/p120847/Main.java
@@ -0,0 +1,13 @@
+package com.ll.level0.p120847;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ src/test/java/com/ll/level2/p87377/Tests.java
@@ -0,0 +1,47 @@
+package com.ll.level2.p87377;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Tests {
+    @Test
+    @DisplayName("교점, [[1, -1, 0], [2, -1, 0]]")
+    void t1() {
+        assertThat(
+                new Solution().intersection(
+                        new int[]{1, -1, 0},
+                        new int[]{2, -1, 0}
+                )
+        ).isEqualTo(
+                new long[]{0, 0}
+        );
+    }
+
+    @Test
+    @DisplayName("교점, [[1, -1, 0], [4, -1, 0]]")
+    void t1_2() {
+        assertThat(
+                new Solution().intersection(
+                        new int[]{1, -1, 0},
+                        new int[]{4, -1, 0}
+                )
+        ).isEqualTo(
+                new long[]{0, 0}
+        );
+    }
+
+    @Test
+    @DisplayName("교점, [[2, -1, 0], [4, -1, 0]]")
+    void t1_3() {
+        assertThat(
+                new Solution().intersection(
+                        new int[]{2, -1, 0},
+                        new int[]{4, -1, 0}
+                )
+        ).isEqualTo(
+                new long[]{0, 0}
+        );
+    }
+}
