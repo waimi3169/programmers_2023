71afa7ea734aa795a6b441f5a5be1daf60f22808
깃waimi3169 <waimi3169@naver.com>
2023-08-09 12:43:29 +0900

    Feat: p181850 풀이

---
+++ src/main/java/com/ll/level0/p181850/Main.java
@@ -0,0 +1,13 @@
+package com.ll.level0.p181850;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+// 실수 flo가 매개 변수로 주어질 때, flo의 정수 부분을 return하도록 solution 함수를 완성해주세요.
+class Solution {
+    public int solution(double flo) {
+        return (int)flo;
+    }
+}
