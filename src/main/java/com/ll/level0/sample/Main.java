bbc2b1b10a68960584f809b04a66e0fedb63ca1d
깃waimi3169 <waimi3169@naver.com>
2024-01-13 22:02:27 +0900

    p181927 풀이 (마지막 두 원소)

--- src/main/java/com/ll/level0/sample/Main.java
+++ src/main/java/com/ll/level0/sample/Main.java
@@ -1,15 +1,25 @@
 package com.ll.level0.sample;
 
-// 단어가 공백 한 개로 구분되어 있는 문자열 my_string이 매개변수로 주어질 때,
-// my_string에 나온 단어를 앞에서부터 순서대로 담은 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+// 정수 리스트 num_list가 주어질 때,
+// 마지막 원소가 그전 원소보다 크면 마지막 원소에서 그전 원소를 뺀 값을
+// 마지막 원소가 그전 원소보다 크지 않다면 마지막 원소를 두 배한 값을 추가하여 return하도록 solution 함수를 완성해주세요.
+
 public class Main {
     public static void main(String[] args) {
 
     }
 }
 class Solution {
-    public String[] solution(String my_string) {
-        String[] answer = {};
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length + 1];
+        for(int i = 0; answer.length > i; i++) {
+            answer[i] = num_list[i];
+            if(num_list[num_list.length-1] > num_list[num_list.length-2]) {
+                answer[answer.length-1] = num_list[num_list.length-1] - num_list[num_list.length-2];
+            } else if(num_list[num_list.length-1] <= num_list[num_list.length-2]) {
+                answer[answer.length-1] = num_list[num_list.length-1]*2;
+            }
+        }
         return answer;
     }
 }
